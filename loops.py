01aaec7fe5d0e6f053026461b3f0912723f1813c
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2022-02-28 18:23:27 +0100

    First

---
+++ loops.py
@@ -0,0 +1,21 @@
+#Print every name in list
+names = ("Nicklas", "Benjamin", "Claus")
+
+for name in names:
+    print(name)
+
+#Sum numbers 1-10
+numbers = [1,2,3,4,5,6,7,8,9,10]
+sum = 0
+
+for number in numbers:
+    sum += number
+
+#For loop in Range
+for i in range(1,6):
+    print(i)
+
+#Counting
+counter = 0
+for i in range(0,6):
+    counter += 1
